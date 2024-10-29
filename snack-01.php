a08d970c219424ff67f8960d13a01e7c2fec778b
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-29 12:26:04 +0100

    feat: comment snack-01

--- snack-01.php
+++ snack-01.php
@@ -1,13 +1,13 @@
 <!-- •	Task: Write a function isPalindrome($str) that takes a string as input and returns true if the string is a palindrome (reads the same backward and forward) and false otherwise. -->
 <!-- •	Example: isPalindrome("madam") should return true, and isPalindrome("hello") should return false. -->
 
-
-
 <?php 
 function isPalindrome($str) {
+    // Base result = false;
     $result = false;
+    // Reverse input string
     $strRev = strrev($str);
-    
+    // If string reversed it's equal to str input it's palindrome result = true
     if ($strRev === $str) {
         $result = true;
     }
