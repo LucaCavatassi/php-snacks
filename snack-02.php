67d14fc94d0ff86afb949430734ab9061cb5ae48
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-29 12:26:50 +0100

    feat: tasks

---
+++ snack-02.php
@@ -0,0 +1,6 @@
+<!-- 2. Fibonacci Sequence Generator
+
+	•	Task: Write a function generateFibonacci($n) that returns the first $n numbers in the Fibonacci sequence.
+	•	Example: generateFibonacci(5) should return [0, 1, 1, 2, 3]. -->
+
+    
