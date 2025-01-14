9ce7bc364019c275d0eda1b41c5e2e69e721e7ce
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-14 15:45:20 +0100

    feat: case with no input

--- snack-20.php
+++ snack-20.php
@@ -1,10 +1,13 @@
 <?php 
-if (isset($_POST["firstNum"]) or isset ($_POST["secondNum"])) {
+var_dump($_POST);
+if (!empty($_POST["firstNum"]) or !empty($_POST["secondNum"])) {
     $operator = $_POST["operator"];
 
     if ($operator === "+") {
-        echo $_POST["firstNum"] + $_POST["secondNum"];
+        echo (int)$_POST["firstNum"] + (int)$_POST["secondNum"];
     } else if ($operator === "-") {
-        echo $_POST["firstNum"] - $_POST["secondNum"];
+        echo (int)$_POST["firstNum"] - (int)$_POST["secondNum"];
     }
+} else {
+    echo "no num provided";
 }
