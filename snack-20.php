9b7b8eb2a9a0971b1162c511c5f5174206e1ae21
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-14 15:40:38 +0100

    feat: snack 1

--- snack-20.php
+++ snack-20.php
@@ -1,1 +1,10 @@
 <?php 
+if (isset($_POST["firstNum"]) or isset ($_POST["secondNum"])) {
+    $operator = $_POST["operator"];
+
+    if ($operator === "+") {
+        echo $_POST["firstNum"] + $_POST["secondNum"];
+    } else if ($operator === "-") {
+        echo $_POST["firstNum"] - $_POST["secondNum"];
+    }
+}
