738441de5946772d0c908700c664229f4f3a6e5c
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-16 15:17:24 +0100

    feat: refined function and php unit

--- snack-22.php
+++ snack-22.php
@@ -1,30 +1,41 @@
 <?php
 $cart = [
-    ["name" => "T-shirt", "price" => 2000.00, "quantity" => 6],
+    ["name" => "T-shirt", "price" => 20.00, "quantity" => 6],
     ["name" => "Sneakers", "price" => 50.00, "quantity" => 1],
     ["name" => "Jeans", "price" => 40.00, "quantity" => 2]
 ];
 
-
 function calculateTotal($cart) {
+    $error_message = "There was an error, please check your cart!";
+
     $total = 0;
-
-    $quantityDiscount = 5;
-    $fullCartDiscount = 10;
-
+    $quantityDiscount = 5; // Sconto del 5% per 5 o più pezzi dello stesso prodotto
+    $fullCartDiscount = 10; // Sconto del 10% per ordini superiori a 100€
+    
     foreach ($cart as $elem) {
-        if($elem["quantity"] > 5) {
-            $elem["price"] = $elem["price"] - ($elem["price"] * ($quantityDiscount / 100));
-        };
-
-        $total += $elem["price"];
+        // Se una delle chiavi del carrello non è settata
+        // O il nome non è una stringa
+        // O il prezzo non è un numero o è minore di 0
+        // O la quantita non è un intero o è minore o uguale a 0
+        // Ritorna errore
+        if (!isset($elem["name"], $elem["price"], $elem["quantity"]) || !is_string($elem["name"]) || !is_numeric($elem["price"]) || $elem["price"] < 0 || !is_int($elem["quantity"]) || $elem["quantity"] <= 0) {
+            return $error_message;
+        }
+        // Il total del singolo elemento prezzo * quantità
+        $itemTotal = $elem["price"] * $elem["quantity"];
+        // Applica lo sconto quantità se ci sono almeno 5 pezzi dello stesso prodotto
+        if ($elem["quantity"] >= 5) {
+            $itemTotal -= $itemTotal * ($quantityDiscount / 100);
+        }
+        // Totale è la somma dei singoli elementi
+        $total += $itemTotal;
     }
-
+    // Applica lo sconto sull'ordine intero se supera i 100€
     if ($total > 100) {
-        $total = $total - ($total * ($fullCartDiscount / 100));
+        $total -= $total * ($fullCartDiscount / 100);
     }
-
-    return number_format((float)$total, 2, ',', '.');
-};
+    // Ritorna il numero formattato con due zeri
+    return number_format($total, 2, ',', '.');
+}
 
 echo calculateTotal($cart);
