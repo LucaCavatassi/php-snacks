ea7ceb2789615284ce4f98a68e507c58c7121449
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-16 15:00:31 +0100

    feat: over 100 discount logic

--- snack-22.php
+++ snack-22.php
@@ -7,13 +7,26 @@ $cart = [
 
 
 function calculateTotal($cart) {
+    $total = 0;
+
     $quantityDiscount = 5;
+    $fullCartDiscount = 10;
+
     foreach ($cart as $elem) {
         if($elem["quantity"] > 5) {
             $elem["price"] = $elem["price"] - ($elem["price"] * ($quantityDiscount / 100));
             var_dump($elem["price"]);
         };
+
+        $total += $elem["price"];
+        var_dump($total);
     }
+
+    if ($total > 100) {
+        $total = $total - ($total * ($fullCartDiscount / 100));
+    }
+
+    return $total;
 };
 
 echo calculateTotal($cart);
