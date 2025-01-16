c6fe5c0febcfa53359e7cc1d9ec3b4ac4b7291a2
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-16 17:23:08 +0100

    feat: oop-6

--- src/OOP-5.php
+++ src/OOP-5.php
@@ -5,6 +5,8 @@ class User {
     public $email;
     public $role;
 
+    public static $users = [];
+
     public function __construct($firstName, $lastName, $email, $role){
         $this->firstName = $firstName;
         $this->lastName = $lastName;
@@ -28,7 +30,6 @@ class User {
 }
 
 class Admin extends User {
-    public $users = [];
 
     public function AddUser($firstName, $lastName, $email, $role) {
         $user = new User($firstName, $lastName, $email, $role);
@@ -42,6 +43,26 @@ class Admin extends User {
     }
 }
 
-class Viewer extends User {
+class Editor extends User {
+    public function editUser($user, $newFirstName = null, $newLastName = null, $newEmail = null) {
+        if ($user !== null) {
+            if ($newFirstName !== null) {
+                $user->firstName = $newFirstName;
+            }
+            if ($newLastName !== null) {
+                $user->lastName = $newLastName;
+            }
+            if ($newEmail !== null) {
+                $user->email = $newEmail;
+            }
+        }
+    }
+}
 
+class Viewer extends User {
+    public function getUsers() {
+        foreach ($this->users as $user) {
+            echo "First Name: {$user->firstName}, Last Name:{$user->lastName}, Email:{$user->email}, Role:{$user->role}";
+        }
+    }
 }
