1a4e5a77453511199d1972705b144c9b3edb43f9
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-17 16:08:45 +0100

    feat: hashed password

--- src/snack-25.php
+++ src/snack-25.php
@@ -2,14 +2,14 @@
 $users = [];
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") { 
-    if (isset($_POST['username']) && isset($_POST['password']) && (strlen($_POST["password"]) > 5)) {
+    if (isset($_POST['username']) && isset($_POST['password']) && (strlen($_POST["password"]) > 5) && (strlen($_POST["username"]) > 5)) {
         $username = $_POST['username'] ?? '';
         $password = $_POST['password'] ?? '';
 
         $newUser = [];
 
         $newUser["username"] = $username;
-        $newUser["password"] = $password;
+        $newUser["password"] = password_hash($password, PASSWORD_BCRYPT);
 
         $users[] = $newUser;
     } else {
