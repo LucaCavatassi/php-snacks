32a5b89670abeed31e751e84824c77232119b791
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-17 16:41:34 +0100

    feat: room refined

--- src/snack-26.php
+++ src/snack-26.php
@@ -9,14 +9,18 @@ class Room {
         $this->bathrooms = $bathrooms;
         $this->available = $available;
     }
-
-    
 }
 
-class Hotel extends Room {
+class Hotel {
     public $allRooms = [];
 
-    function checkAvailability () {
+    // Aggiungi una stanza all'hotel
+    public function addRoom($room) {
+        $this->allRooms[] = $room;
+    }
+
+    // Verifica disponibilità stanze
+    public function checkAvailability() {
         $availableRooms = [];
 
         foreach ($this->allRooms as $room) {
@@ -28,24 +32,61 @@ class Hotel extends Room {
         return $availableRooms;
     }
 
-    function bookRoom($room) {
-        if(array_search($room, $this->allRooms) && $room->available === true){
-            $room->available = false;
-        }
-        return "Room {$room} is booked!";
-    }
-
-    function unBookRoom($room) {
-        if(array_search($room, $this->allRooms) && $room->available === false){
-            $room->available = true;
+    // Prenota una stanza
+    public function bookRoom($room) {
+        // Verifica se la stanza è disponibile
+        if (($key = array_search($room, $this->allRooms)) !== false && $room->available === true) {
+            $this->allRooms[$key]->available = false;
+            return "Room booked!";
         }
 
-        return "Room {$room} is unbooked!";
+        return "Room is not available or does not exist.";
     }
 
-    function checkRooms() {
+    // Annulla prenotazione
+    public function unBookRoom($room) {
+        if (($key = array_search($room, $this->allRooms)) !== false && $room->available === false) {
+            $this->allRooms[$key]->available = true;
+            return "Room unbooked!";
+        }
+
+        return "Room was not booked or does not exist.";
+    }
+
+    // Controlla tutte le stanze
+    public function checkRooms() {
+        $roomStatus = [];
         foreach ($this->allRooms as $room) {
-            return $room->avilable;
+            $roomStatus[] = [
+                'rooms' => $room->rooms,
+                'bathrooms' => $room->bathrooms,
+                'available' => $room->available
+            ];
         }
+        return $roomStatus;
     }
 }
+
+// Esempio d'uso
+$hotel = new Hotel();
+
+$room1 = new Room(1, 1, true);
+$room2 = new Room(2, 1, true);
+$room3 = new Room(3, 2, false);
+
+$hotel->addRoom($room1);
+$hotel->addRoom($room2);
+$hotel->addRoom($room3);
+
+// Prenotazione di una stanza
+echo $hotel->bookRoom($room1);  // Room booked!
+echo "\n";
+
+// Verifica disponibilità
+$availableRooms = $hotel->checkAvailability();
+print_r($availableRooms);
+
+// Controlla tutte le stanze
+$rooms = $hotel->checkRooms();
+print_r($rooms);
+?>
