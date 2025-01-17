20c16fd39d4524d4af97ad2120da1d2bd9231249
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-17 16:12:03 +0100

    feat: refined version of user saving

--- src/snack-25.php
+++ src/snack-25.php
@@ -1,20 +1,38 @@
 <?php 
+$filename = "users.json";
 $users = [];
 
+// Verifica se il file esiste e carica i dati se presenti
+if (file_exists($filename)) {
+    $users = json_decode(file_get_contents($filename), true);
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") { 
-    if (isset($_POST['username']) && isset($_POST['password']) && (strlen($_POST["password"]) > 5) && (strlen($_POST["username"]) > 5)) {
-        $username = $_POST['username'] ?? '';
-        $password = $_POST['password'] ?? '';
+    if (isset($_POST['username']) && isset($_POST['password']) && strlen($_POST["password"]) > 5 && strlen($_POST["username"]) > 5) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-        $newUser = [];
+        // Crea il nuovo utente
+        $newUser = [
+            "username" => $username,
+            "password" => password_hash($password, PASSWORD_BCRYPT)
+        ];
 
-        $newUser["username"] = $username;
-        $newUser["password"] = password_hash($password, PASSWORD_BCRYPT);
-
+        // Aggiungi l'utente al file JSON
         $users[] = $newUser;
+
+        // Salva di nuovo nel file
+        file_put_contents($filename, json_encode($users));
+
+        echo "User added successfully.";
     } else {
-        echo "Please check the fields something is missing.";
+        echo "Please check the fields; something is missing or too short.";
     }
 } else {
     echo "Invalid request method.";
 }
+
+// Mostra gli utenti in formato JSON
+header('Content-Type: application/json');
+echo json_encode($users); 
+?>
